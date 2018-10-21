6d2babc0edc33bfefa5e98e9acd60a7925af5cb8
barmaleysk <43924454+barmaleysk@users.noreply.github.com>
2018-10-21 15:28:01 +0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
     TOKEN: '586419223:AAHkzS27NeGSh7O1W73uAKqiqOEJHcEdcu4',
-    DB_URL: 'mongodb://localhost/darkwarehouse'
+    DB_URL: 'mongodb://user:Gewphyobvaddos0@ds137643.mlab.com:37643/garant'
 }
